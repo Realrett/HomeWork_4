bbb55a734a161e939d605f1f29f4f009a73a38b8
Realrett <gosha-student@mail.ru>
2022-10-29 23:57:44 +0700

    Решил задачу 2

---
+++ Exersize2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int GetConsole (string message)
+{
+    int result;
+
+    while(true) 
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out int number)) 
+        {
+            result = number;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число, повторите ввод \n");
+        }
+    }
+
+    return result;
+}
+
+int A = GetConsole("Введите число: ");
+int B = A;
+int sum = 0;
+while (A > 0)
+{
+sum = sum + A % 10;
+A = A /10 ;
+}
+
+Console.Write($"Сумма цифр числа {B} равна {sum}");
